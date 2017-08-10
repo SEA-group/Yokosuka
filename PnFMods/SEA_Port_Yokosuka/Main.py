1cdef3e49d8495608e6914f29e98ae0e95c2aacc
AstreTunes <mebius.uranus@gmail.com>
2017-08-10 21:41:43 +0200

    v1.0.5

--- PnFMods/SEA_Port_Yokosuka/Main.py
+++ PnFMods/SEA_Port_Yokosuka/Main.py
@@ -1,3 +1,3 @@
 API_VERSION = 'API_v1.0'
 
-customPorts.addCustomPort("Dock_Japan", portDisplayName = "Yokosuka", bargePos = None)
+customPorts.addCustomPort("Dock_Japan", portDisplayName = "Yokosuka", bargePos = ( (-15,0,4.3) , 1.57075 , (-3.6,3.9,1.8)  ))
